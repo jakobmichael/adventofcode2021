2f11c9e7d14756e279441451aa6bf8ea7a271499
Jakob <jakob_luge@mailbox.org>
2021-12-06 21:40:40 +0100

    finish day 6

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -13,15 +13,17 @@ def init_state(filename):
 input = "day_6_input.txt"
 test_input = "test_6_input.txt"
 
-number_of_fish = init_state(input)
+number_of_fish = list(init_state(input))
+
 
 day = 0
-while day < 100:
-    print(day)
+"""
+while day < 10:
+    #print("\r"+day.__str__())
 
     upper_bound = number_of_fish.__len__()
     new_fish = []
-    for i in range(0, upper_bound):
+    for i,fish in enumerate(number_of_fish):
         if(number_of_fish[i] == 0):
             number_of_fish[i] = 6
             new_fish.append(8)
@@ -31,7 +33,44 @@ while day < 100:
     for fish in new_fish:
         number_of_fish.append(fish)
     day += 1
-    print(day)
 
 
 print(number_of_fish.__len__())
+
+"""
+#Part Two
+
+print('Part Two')
+
+timer_list = {0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0}
+
+
+for fish in number_of_fish:
+    timer_list[fish] += 1
+
+while day < 256:
+    print(timer_list)
+    temp_0 = timer_list[1]
+    timer_list[1] = timer_list[2]
+    timer_list[2] = timer_list[3]
+    timer_list[3] = timer_list[4]
+    timer_list[4] = timer_list[5]
+    timer_list[5] = timer_list[6]
+    timer_list[6] = timer_list[7] + timer_list[0]
+    timer_list[7] = timer_list[8]
+    timer_list[8] = timer_list[0]
+    timer_list[0] = temp_0
+
+    day += 1
+
+
+sum = 0
+
+#print(timer_list)
+
+for key, value in timer_list.items():
+    sum += value
+
+
+print(sum)
+
