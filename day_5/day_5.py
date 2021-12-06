02c0732183f70f5910224d07cd1911e416be32ec
jakobmichael <jakob_luge@mailbox.org>
2021-12-06 11:24:44 +0100

    finish day5 part one

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,9 +1,5 @@
 from sys import stdout
-import matplotlib.pyplot as plt
-import numpy as np
-import itertools
-from shapely.geometry import LineString
-from shapely.geometry import Point
+
 
 # Part One
 print("Solution Part One")
@@ -19,24 +15,24 @@ with open(input) as file:
         plot_line = []
         for str in coordinates:
             splitted_str = str.split(",")
-            for i in range(0,splitted_str.__len__()):
+            for i in range(0, splitted_str.__len__()):
                 splitted_str[i] = int(splitted_str[i])
-                
-            
-            plot_line.append(splitted_str)
-            
-        line_coordinates.append(plot_line)
-     
 
-def check_points_in_grid(x1,x2,grid):
+            plot_line.append(splitted_str)
+
+        line_coordinates.append(plot_line)
+
+
+def check_points_in_grid(x1, x2, grid):
     key_check = x1.__str__() + '/' + x2.__str__()
+    """
     for point in grid:
         key = list(point.keys())[0]
         if(key == key_check):
             point[key] += 1
-           
+    """
 
-
+    grid[key_check] += 1
 
 
 def generate_valid_lines():
@@ -46,17 +42,17 @@ def generate_valid_lines():
         y1 = line[0][1]
 
         x2 = line[1][0]
-        y2 = line[1][1]  
+        y2 = line[1][1]
 
         if(x1 == x2):
             points_in_line = []
             if(y1 < y2):
-              
-                for y in range(y1,y2+1):
+
+                for y in range(y1, y2+1):
                     points_in_line.append([x1, y])
             else:
-               
-                for y in range(y2,y1+1):
+
+                for y in range(y2, y1+1):
                     points_in_line.append([x1, y])
 
             valid.append(points_in_line)
@@ -64,12 +60,12 @@ def generate_valid_lines():
         if(y1 == y2):
             points_in_line = []
             if(x1 < x2):
-               
-                for x in range(x1,x2+1):
+
+                for x in range(x1, x2+1):
                     points_in_line.append([x, y1])
             else:
-                
-                for x in range(x2,x1+1):
+
+                for x in range(x2, x1+1):
                     points_in_line.append([x, y1])
 
             valid.append(points_in_line)
@@ -77,17 +73,10 @@ def generate_valid_lines():
     return valid
 
 
-
 def generate_empty_grid():
-    for x in range(0,max_x+1):
+    for x in range(0, max_x+1):
         for y in range(0, max_y+1):
-            line_grid.append({x.__str__() +'/' +y.__str__() : 0})
-
-
-
-
-
-
+            line_grid[x.__str__() + '/' + y.__str__()] = 0
 
 
 def get_x_y_vals(coordinates):
@@ -96,6 +85,7 @@ def get_x_y_vals(coordinates):
             x.append(points[0])
             y.append(points[1])
 
+
 x = []
 y = []
 
@@ -103,7 +93,7 @@ get_x_y_vals(line_coordinates)
 
 max_x = max(x)
 max_y = max(y)
-line_grid = []
+line_grid = {}
 
 
 generate_empty_grid()
@@ -111,35 +101,23 @@ generate_empty_grid()
 valid_lines = generate_valid_lines()
 
 
-for index,line in enumerate(valid_lines):
-    stdout.write("\r"+index.__str__() + " of " + valid_lines.__len__().__str__())
-    stdout.write("\n") 
-    for i,points in enumerate(line):
+for index, line in enumerate(valid_lines):
+    stdout.write("\r"+index.__str__() + " of " +
+                 valid_lines.__len__().__str__())
+    stdout.write("\n")
+    for i, points in enumerate(line):
         stdout.write("\r"+i.__str__() + " of " + line.__len__().__str__())
         stdout.flush()
-        check_points_in_grid(points[0],points[1],line_grid)
-    stdout.write("\n") 
+        check_points_in_grid(points[0], points[1], line_grid)
+    stdout.write("\n")
+
 
 valid_overlaps = 0
 
 
-for field in line_grid:
-    key = list(field.keys())[0]
-    if(field[key] > 1):
-       valid_overlaps += 1
-
+for key, value in line_grid.items():
+    if(value > 1):
+        valid_overlaps += 1
 
 
 print(valid_overlaps)
-
-
-
-
-
-
-
-       
-
-
-
-
