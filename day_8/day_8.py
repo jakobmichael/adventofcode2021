aa2a85d60f26ee06eddf396a9b7f3e6650557cec
Jakob-Michael Luge <Jakob-Michael.Luge@sivis.com>
2021-12-09 10:01:02 +0100

    finish day 8

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -31,13 +31,15 @@ def get_number_of_instances(segment_length, dict):
     return instances
 
 def get_number_decoding(segment_length, dict):
-    decoded = ""
-    for key,value in dict.items():
-        for code in value["entry"]:
-            if len(code) == segment_length:
-                decoded = code
-                break
-       
+    decoded = []
+    for code in dict["entry"]:
+        if len(code) == segment_length:
+            decoded.append(code)
+            
+    for code in dict["output"]:
+        if len(code) == segment_length:
+            decoded.append(code)
+            
     return decoded
 
 
@@ -46,7 +48,7 @@ input = "day_8_input.txt"
 test_input_1 = "test_8_input_1.txt"
 test_input_2 = "test_8_input_2.txt"
 
-input_data = init_state(test_input_2)
+input_data = init_state(input)
 
 
 
@@ -70,36 +72,148 @@ print(instances)
 
 
 
+def split(word):
+    return [char for char in word]
+
+
+
+
+char_lengths = {
+    2: None,
+    3: None,
+    4: None,
+    6: None,
+    5: None,
+    7: None,
     
-
-
-
-char_decodings = {
-    1: "",
-    2: "",
-    3: "",
-    4: "",
-    5: "",
-    6: "",
-    7: "",
-    8: "",
-}
-
-char_positions = {
-    1: "",
-    2: "",
-    3: "",
-    4: "",
-    5: "",
-    6: "",
-    7: "",
 }
 
 
-for key,value in needed_segments.items():
-    char_decodings[key] = get_number_decoding(value, input_data)
 
 
+def get_char_lengths(input):
+
+    for key,value in char_lengths.items():
+        char_lengths[key] = (get_number_decoding(key,input))
+
+
+
+def decode_chars():
+
+    decoded_chars = {
+    0: [],
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: []
+    }   
+
+    for key,values in char_lengths.items():
+
+        for value in values:
+
+            one_elements = []
+            four_elements = []
+            nine_elements = []
+
+            contains_one = 0
+            contains_four = 0
+            contains_nine = 0
+
+            if key == 7:
+                decoded_chars[8].append(value)
+
+            if key == 2:
+                decoded_chars[1].append(value)
+    
+            if key == 3:
+                decoded_chars[7].append(value)
+
+            if key == 4:
+                decoded_chars[4].append(value)
+
+            if key == 6:
+                one_elements = split(decoded_chars[1][0])
+                four_elements = split(decoded_chars[4][0])
+        
+                contains_one = 0
+                contains_four = 0  
+
+                for char in one_elements:
+                    if char in value:
+                        contains_one += 1
+        
+                for char in four_elements:
+                    if char in value:
+                     contains_four += 1
+        
+                if contains_one == 1:
+                    decoded_chars[6].append(value)
+        
+                elif contains_four == 3:
+                    decoded_chars[0].append(value)
+        
+                else:
+                    decoded_chars[9].append(value)
     
 
-print(char_decodings)
+            if key == 5:
+
+                one_elements = split(decoded_chars[1][0])
+                nine_elements = split(decoded_chars[9][0])
+
+                for char in one_elements:
+                    if char in value:
+                        contains_one += 1
+
+                for char in nine_elements:
+                    if char in value:
+                        contains_nine += 1
+        
+                if contains_one == 2:
+                    decoded_chars[3].append(value)
+        
+                elif contains_nine == 5:
+                    decoded_chars[5].append(value)
+        
+                else:
+                    decoded_chars[2].append(value)
+
+    return decoded_chars
+
+
+
+total_sum = []
+
+def decode_output(input,decoded):
+    sum = ""
+    for code in input["output"]:
+        for key,value in decoded.items():
+            if code in value:
+                sum += str(key)
+
+    total_sum.append(sum)
+
+def sum_all_output_sums():
+    output_sum = 0
+    for str in total_sum:
+        output_sum += int(str)
+
+    return output_sum
+
+for key, value in input_data.items():
+    get_char_lengths(value)
+    decoded = decode_chars()
+    decode_output(value,decoded)
+
+
+
+
+
+
+print(sum_all_output_sums())
